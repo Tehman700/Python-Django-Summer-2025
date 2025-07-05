b53d709c7569bbec3df5ff7d749d44a1b55046c8
Tehman700 <tehmanfast@gmail.com>
2025-07-06 00:12:53 +0500

    .

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# registering to model
+
+from .models import Room
+
+admin.site.register(Room)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 
@@ -20,4 +21,5 @@ def home(request):
     return render(request, 'home.html')
 
 def rooms(request):
-    return render(request, 'rooms.html')
+    rooms_var = Room.objects.all()
+    return render(request, 'rooms.html', {'i' :rooms_var})
