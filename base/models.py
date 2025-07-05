16631d44bc5d6069453e65af8b554025f366f302
Tehman700 <tehmanfast@gmail.com>
2025-07-05 23:47:18 +0500

    created database and tables

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# Models are classes which are tables
+
+
+class Room(models.Model):
+    #host = 
+    #topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null =True, blank = True)
+
+    #participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    
