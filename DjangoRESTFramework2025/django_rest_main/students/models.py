edee78779fc03b298714a4261f913b30c7a1bf55
Tehman700 <tehmanfast@gmail.com>
2025-07-07 03:41:58 +0500

    .

--- DjangoRESTFramework2025/django_rest_main/students/models.py
+++ DjangoRESTFramework2025/django_rest_main/students/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+
+    student_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    branch = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
