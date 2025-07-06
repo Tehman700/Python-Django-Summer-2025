d7cbb3be6dffcee5552f07bfc123b084ebe62ffa
Tehman700 <tehmanfast@gmail.com>
2025-07-07 03:25:44 +0500

    .

--- DjangoRESTFramework2025/django_rest_main/students/views.py
+++ DjangoRESTFramework2025/django_rest_main/students/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def students(request):
+
+    students_data = [
+        {
+            'id' : 1,
+            'name' : "Tehman Hassan",
+            'batch' : 2023
+        }
+    ]
+
+    return HttpResponse(students_data)
