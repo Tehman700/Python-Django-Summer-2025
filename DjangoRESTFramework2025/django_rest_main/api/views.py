62738b941632c2698861821851bb4610343cbf77
Tehman700 <tehmanfast@gmail.com>
2025-07-07 04:02:52 +0500

    Now posting the API Chekcing

--- DjangoRESTFramework2025/django_rest_main/api/views.py
+++ DjangoRESTFramework2025/django_rest_main/api/views.py
@@ -25,7 +25,7 @@ from rest_framework.decorators import api_view
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def studentsView(request):
 
     if request.method == 'GET':
@@ -36,3 +36,14 @@ def studentsView(request):
         serializer = StudentSerializer(students, many =True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
